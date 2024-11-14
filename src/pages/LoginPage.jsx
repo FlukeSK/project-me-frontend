49ad06a55706d654d734f4cb01d7bd411b245ed6
FlukeSK <dszoneworkforme@gmail.com>
2024-11-14 21:06:22 +0700

    update bg login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,7 +7,15 @@ import Footer from "../layouts/Footer";
 // =============== < Function > =============== //
 export default function LoginPage() {
   return (
-    <div>
+    <div
+      className=""
+      style={{
+        backgroundSize: "2000px 2000px",
+        backgroundImage:
+          "url(https://img.freepik.com/free-vector/blurred-valentine-s-day-wallpaper_23-2148819570.jpg?w=996&t=st=1710775724~exp=1710776324~hmac=5d64db4d8645a2be4c6bfeedb15cc1b7fd02d444c9822719a2493273ada71e80)",
+      }}
+    >
+      
       <br />
           <LogoCat />
     <div className="max-w-sm mx-auto my-6 rounded-lg p-4 bg-white shadow-lg">
